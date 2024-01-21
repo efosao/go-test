3d4f3c233cd38f7b1e18c9f2a42304d85674ad38
Efosa Oyegun <852788+efosao@users.noreply.github.com>
2024-01-20 17:53:30 -0800

    added pages and templates to gofiber

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gofiber/models"
 	"log"
+	"time"
 
 	"github.com/Pacific73/gorm-cache/cache"
 	"github.com/Pacific73/gorm-cache/config"
@@ -15,8 +16,6 @@ import (
 )
 
 func main() {
-	engine := mustache.New("./views", ".mustache")
-
 	dsn := "host=efosa.me user=postgres password=5005227a52c02361b7e95a1f5acfc7f0 dbname=jobs_db port=44553 sslmode=disable TimeZone=America/Los_Angeles"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -31,28 +30,29 @@ func main() {
 		CacheMaxItemCnt:      5,     // if length of objects retrieved one single time
 		// exceeds this number, then don't cache
 	})
-
 	db.Use(cache)
 
 	sqlDB, err := db.DB()
-	sqlDB.SetMaxIdleConns(20)
-	sqlDB.SetMaxOpenConns(30)
-
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// Or from an embedded system
-	//   Note that with an embedded system the partials included from template files must be
-	//   specified relative to the filesystem's root, not the current working directory
-	// engine := mustache.NewFileSystem(http.Dir("./views", ".mustache"), ".mustache")
+	sqlDB.SetMaxIdleConns(20)
+	sqlDB.SetMaxOpenConns(30)
 
-	// Pass the engine to the Views
+	engine := mustache.New("./views", ".mustache")
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
 	app.Use(compress.New())
+
+	app.Static("/public", "./public", fiber.Static{
+		Compress:      true,
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+		ByteRange:     true,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		// Render index within layouts/main
@@ -70,19 +70,24 @@ func main() {
 		// }, "layouts/main")
 
 		posts := &[]models.Post{}
-		db.Select("Id", "Title", "CreatedAt").Limit(10).Order(clause.OrderByColumn{Column: clause.Column{Name: "created_at"}, Desc: true}).Find(&posts)
+		db.Select("ID", "CompanyName", "Location", "Tags", "Thumbnail", "Title", "PublishedAt", "CreatedAt").Limit(8).Order(clause.OrderByColumn{Column: clause.Column{Name: "created_at"}, Desc: true}).Find(&posts)
+
 		return c.Render("posts", fiber.Map{
 			"Title": "Job Posts",
 			"Posts": posts,
 		}, "layouts/main")
 	})
 
-	app.Get("/single", func(c *fiber.Ctx) error {
-		// Render w/o layout
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
+	app.Get("/partials/posts/details/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		post := &models.Post{}
+		db.Select("ID", "Title", "Description").Where(&models.Post{ID: id}).First(&post)
+		return c.Render("post_details", fiber.Map{
+			"Title":       post.Title,
+			"ID":          post.ID,
+			"Description": post.GetDescription(),
 		})
 	})
 
-	log.Fatal(app.Listen(":8000"))
+	log.Fatal(app.Listen("localhost:8000"))
 }
--- models/post.go
+++ models/post.go
@@ -2,11 +2,61 @@ package models
 
 import (
 	"time"
+
+	"fmt"
+
+	"github.com/go-mods/initials"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+	"github.com/lib/pq"
 )
 
 type Post struct {
-	ID          string `gorm:"primaryKey;default:cuid()"`
-	Title       string
-	Description string
-	CreatedAt   time.Time
+	ID              string `gorm:"primaryKey;default:cuid()"`
+	CompanyName     string
+	CompanyInitials string `gorm:"-"`
+	Location        string
+	Tags            pq.StringArray `gorm:"type:text[]"`
+	Thumbnail       string
+	Title           string
+	Description     string
+	PublishedAt     time.Time
+	CreatedAt       time.Time
+}
+
+func (p Post) GetInitials() string {
+	return initials.GetInitials(p.CompanyName)
+}
+
+func (p Post) TimeSinceCreated() string {
+	timespan := time.Since(p.PublishedAt).Hours()
+	switch {
+	case timespan < 24:
+		return "Today"
+	case timespan < 48:
+		return "Yesterday"
+	case timespan < (24 * 30):
+		return fmt.Sprintf("%d %s", int(timespan/24), "days ago")
+	default:
+		return fmt.Sprintf("%d %s", int(timespan/(24*30)), "months ago")
+	}
+}
+
+func mdToHTML(md []byte) []byte {
+	// create markdown parser with extensions
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse(md)
+
+	// create HTML renderer with extensions
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
+}
+
+func (p Post) GetDescription() string {
+	return string(mdToHTML([]byte(p.Description)))
 }
