a5e2895cae0a2752ccd686cdd9b064a27e80f617
Efosa Oyegun <852788+efosao@users.noreply.github.com>
2024-03-12 08:55:54 -0700

    adjust menu styles

--- components/navbar.go
+++ components/navbar.go
@@ -29,7 +29,7 @@ func Navbar(config *models.Config) g.Node {
 					),
 				),
 				h.Div(
-					h.Class("flex items-center lg:order-2"),
+					h.Class("hidden lg:flex items-center lg:order-2"),
 					h.A(
 						h.Href("#"),
 						h.Class("text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"),
@@ -48,10 +48,10 @@ func Navbar(config *models.Config) g.Node {
 					),
 				),
 				h.Div(
-					h.Class("hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"),
+					h.Class("flex justify-between items-center w-auto order-1"),
 					h.ID("mobile-menu-2"),
 					h.Ul(
-						h.Class("flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"),
+						h.Class("flex font-medium flex-row gap-4"),
 						NavLink("/", "Home", currentPath),
 						NavLink("/about", "About", currentPath),
 						NavLink("/posts", "Job Posts", currentPath),
@@ -117,7 +117,7 @@ func NavLink(href, name, currentPath string) g.Node {
 		h.A(
 			h.Href(href),
 			c.Classes{
-				"block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700": true,
+				"block p-2 border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700": true,
 				"text-gray-700 dark:text-gray-400": currentPath != href,
 				"text-white dark:text-gray-200":    currentPath == href,
 			},
